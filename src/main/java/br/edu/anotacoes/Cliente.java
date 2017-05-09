6363e902d298eb97908b2b4654d86b4ba2d6e017
Andre <>
2017-05-08 21:21:06 -0300

    Criação do Projeto

---
+++ src/main/java/br/edu/anotacoes/Cliente.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.anotacoes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Lab1
+ */
+
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+    //Atributos
+    //Id do cliente
+    @Id
+    @GeneratedValue
+    private int id;
+    
+    //nome
+    @Column(length = 50, nullable = false)
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+}
