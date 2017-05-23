61a8d81d74dd9d18c33725a8e1b3446c5d70b728
Andre <>
2017-05-22 21:29:33 -0300

    Todo o cadastrar Especialidade

---
+++ src/main/java/br/edu/DAO/EspecialidadeDAO.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.DAO;
+
+import br.edu.anotacoes.Especialidade;
+
+/**
+ *
+ * @author Lab3
+ */
+public class EspecialidadeDAO extends GenericoDAO<Especialidade>{
+
+    public EspecialidadeDAO() {
+        super(Especialidade.class);
+    }
+    
+}
---
+++ src/main/java/br/edu/anotacoes/Especialidade.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.anotacoes;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Lab3
+ */
+@Entity
+public class Especialidade {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String especialidade;
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public Especialidade() {
+    }
+
+    public Especialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+    
+    
+    
+}
