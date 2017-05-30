7f5d3a4ab20501f028de57548475a87f6f42ff14
Lab1 <Lab1@Lab1-24>
2017-05-30 17:58:13 -0300

    Alteraçoes na lista de atendimento para medico

--- src/main/java/br/edu/DAO/AtendimentoDAO.java
+++ src/main/java/br/edu/DAO/AtendimentoDAO.java
@@ -6,7 +6,11 @@
 package br.edu.DAO;
 
 import br.edu.anotacoes.Atendimento;
+import br.edu.util.Validacao;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
+import javax.swing.JOptionPane;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 
@@ -36,12 +40,35 @@ public class AtendimentoDAO extends GenericoDAO<Atendimento>{
         return lista;
     }
 
-    public List<Atendimento> listarGenerico(String campo, Object valor) {
+    public List<Atendimento> listarHoje(String campo, Object valor) {
+        Validacao validar = new Validacao();
         List<Atendimento> lista = null;
         try {
             sessao = getSessao().openSession();
-            lista = sessao.createCriteria(classe).
+            lista = sessao.createCriteria(Atendimento.class).
+                    add(Restrictions.eq("data", validar.converterIdade(java.text.DateFormat.getDateInstance(DateFormat.MEDIUM).format(new Date())))).
                     add(Restrictions.eq(campo, valor)).
+                    addOrder(Order.asc("data")).
+                    addOrder(Order.asc("horaConsulta")).
+                    list();
+        } catch (Exception e) {
+            System.out.println("Erro na lista de campo: " + e);
+        }
+        finally{
+            sessao.close();
+        }
+        return lista;
+    }
+    
+    public List<Atendimento> listarDoisCampos(String campo, Object valor, String campo2, Object valor2) {
+        Validacao validar = new Validacao();
+        List<Atendimento> lista = null;
+        try {
+            sessao = getSessao().openSession();
+            lista = sessao.createCriteria(Atendimento.class).
+                    add(Restrictions.eq(campo, valor)).
+                    add(Restrictions.eq(campo2, valor2)).
+                    addOrder(Order.asc("data")).
                     addOrder(Order.asc("horaConsulta")).
                     list();
         } catch (Exception e) {
